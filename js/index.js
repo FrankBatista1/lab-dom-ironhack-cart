fd0eeca39ccac554af98d0e40da1b2c211139364
FrankBatista1 <fb53901@gmail.com>
2021-08-23 21:28:38 -0400

    Attempt to change the price

--- js/index.js
+++ js/index.js
@@ -1,23 +1,42 @@
 // ITERATION 1
 
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML//gueting de price from the HTML 
+  const quantity = product.querySelector('.quantity input').value//guetting the quantity from the HTML
+  const subtotalIs = price * quantity
+  const defaultSubtotal = product.querySelector('.subtotal span').innerHTML = subtotalIS
+  defaultSubtotal = subtotalIs
+  
+  product.querySelector('subtotal span').innerHTML
+  return defaultSubtotal;
 }
-
-function calculateAll() {
+function calculateAll(product) {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
-
+  
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll('.product');
+  products.forEach(product) => {
+    updateSubtotal(product);//apllies the function no every index
+  }
+
+
+  
 
   // ITERATION 3
-  //... your code goes here
+  let total = 0 
+  products.forEach(product) => {
+    subtotal = product.childNodes[7].childNodes[1].innerHTML
+    console.log('Subtotal:',subtotal);
+    total += Number(subtotal)
+  }
+  document.getElementsById('total-value')
+  document.getelem
 }
 
 // ITERATION 4
@@ -34,9 +53,9 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
+window.addEventListener('click', (() => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
-});
+}));
